0f5a83a9066ee8a458a3cf9635bbb19c087fabfd
Faishal Pasa <faishal.pasa@bareksa.com>
2024-05-03 16:01:50 +0700

    feat: change content

--- src/App.tsx
+++ src/App.tsx
@@ -83,6 +83,18 @@ function App() {
     window.open(link, '_blank')
   }
 
+  const handleOpenSocial = (type: string) => {
+    if (type === 'linkedin') {
+      window.open('https://www.linkedin.com/in/faishalpasa/', '_blank')
+    } else if (type === 'github') {
+      window.open('https://github.com/faishalpasa/', '_blank')
+    } else if (type === 'instagram') {
+      window.open('https://www.instagram.com/faishalpasa/', '_blank')
+    } else if (type === 'email') {
+      window.open('mailto:mfaishalpasa@gmail.com', '_blank')
+    }
+  }
+
   const runTypeWriter = (text: string, index: number, callBack: any) => {
     let newWelcomeText = ''
     let i = 0
@@ -167,8 +179,8 @@ function App() {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-600">
-      <header className="bg-gray-950 p-4 md:p-6 flex justify-center">
-        <div className="flex gap-4 w-full md:max-w-screen-sm">
+      <header className="bg-gray-950 p-4 md:p-6">
+        <div className="flex gap-4 w-full md:max-w-screen-sm m-auto">
           <div className="w-[70px] sm:w-[150px]">
             <Lottie
               options={defaultOptions}
@@ -182,21 +194,65 @@ function App() {
               <span className="text-base md:text-2xl font-semibold" dangerouslySetInnerHTML={{ __html: texts?.[0] }} />
               <span className="text-base md:text-2xl font-semibold text-orange-400" dangerouslySetInnerHTML={{ __html: texts?.[1] }} />
               {textIndex <= 1 && (
-              <span className="animate-typeCarret text-base md:text-2xl">|</span>
+                <span className="animate-typeCarret text-base md:text-2xl">|</span>
               )}
             </p>
             <p className="mt-1 sm:mt-2">
               <span className="text-sm md:text-xl font-normal" dangerouslySetInnerHTML={{ __html: texts?.[2] }} />
               <span className="text-sm md:text-xl font-normal" dangerouslySetInnerHTML={{ __html: repeatedText }} />
               {textIndex > 1 && (
-              <span className="animate-typeCarret text-sm md:text-xl">|</span>
+                <span className="animate-typeCarret text-sm md:text-xl">|</span>
               )}
             </p>
           </div>
         </div>
+
+        <div className="mt-4 flex gap-6 text-white w-full md:max-w-screen-sm m-auto justify-center">
+          <span className="[&>svg]:h-5 [&>svg]:w-5 cursor-pointer transform transition duration-500 ease-in-out hover:scale-150" onClick={() => handleOpenSocial('email')} role="presentation">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+          </span>
+
+          <span className="[&>svg]:h-5 [&>svg]:w-5 cursor-pointer transform transition duration-500 ease-in-out hover:scale-150" onClick={() => handleOpenSocial('linkedin')} role="presentation">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 448 512"
+            >
+              <path
+                d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
+              />
+            </svg>
+          </span>
+
+          <span className="[&>svg]:h-5 [&>svg]:w-5 cursor-pointer transform transition duration-500 ease-in-out hover:scale-150" onClick={() => handleOpenSocial('github')} role="presentation">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 496 512"
+            >
+              <path
+                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+              />
+            </svg>
+          </span>
+
+          <span className="[&>svg]:h-5 [&>svg]:w-5 cursor-pointer transform transition duration-500 ease-in-out hover:scale-150" onClick={() => handleOpenSocial('instagram')} role="presentation">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 448 512"
+            >
+              <path
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+              />
+            </svg>
+          </span>
+        </div>
       </header>
-      <main className="m-auto p-6 flex-1 max-w-screen-lg">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch gap-4 md:gap-6">
+      <main className="m-auto sm:p-6 p-0 flex-1 max-w-screen-lg">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch gap-4 md:gap-6 bg-gray-700 sm:rounded-lg p-4 sm:p-6">
           {PORTFOLIOS.map((portfolio) => (
             <div key={portfolio.link} role="presentation" className="cursor-pointer card bg-gray-900 rounded-lg shadow-md transform transition duration-500 ease-in-out hover:scale-105" onClick={() => handleOpenLink(portfolio.link)}>
               <img src={portfolio.imgSrc} alt="portfolio" className="w-full h-36 md:h-48 object-cover rounded-lg" />
@@ -208,6 +264,7 @@ function App() {
             </div>
           ))}
         </div>
+        {/* <div className="mt-6 bg-gray-700 grid grid-cols-3 items-stretch gap-4 rounded-lg p-4" /> */}
       </main>
       <footer className="bg-gray-950 p-4 text-white text-center">
         <p className="text-xs">
